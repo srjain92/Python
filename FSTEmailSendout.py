3a0bc802467a8c0313b00d45c7a4641f95bf8457
Saurabh Jain <saurabhjainjs@gmail.com>
2020-06-09 15:33:12 -0400

    Update FSTEmailSendout.py

--- FSTEmailSendout.py
+++ FSTEmailSendout.py
@@ -1,22 +1,19 @@
+#Code to send out an email with an attached file
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
 
-filename = r'\\dds-file04\dds lab\Salesforce Issues Log\FST Detail Information Data Extract.xlsx'
-# filename = 'S:\\Salesforce Issues Log\FST Detail Information Data Extract.xlsx'
+filename = r'\\filename.xlsx'
 
 # Email sending code
-email_user = 'saurabh.jain@ddslab.com'
-email_password = 'J@in2018oct'
-email_list = ['Rose.Vazquez@ddslab.com', 'Kent.Billiter@ddslab.com', 'lacey.mitchell@ddslab.com', 'Petar.Andjelic@ddslab.com',
-              'Marge.Nicholson@ddslab.com', 'Michael.Radke@ddslab.com', 'Katrina.Dahdah@ddslab.com', 'Preston.Bryan@ddslab.com',
-              'Nicolle.Makle@ddslab.com', 'Michelle.Brockman@ddslab.com', 'Brianna.Voshell@ddslab.com', 'Terrance.Hopson@ddslab.com'
-              , 'Terry.Davis@ddslab.com', 'Angela.Warren@ddslab.com', 'Brittany.Diehl@ddslab.com', 'saurabh.jain@ddslab.com']
+email_user = 'abc@gmail.com'
+email_password = 'xxx'
+email_list = ['abc@gmail.com', 'abc@gmail.com']
 email_send = ', '.join(email_list)
 
-subject = 'FST Underlying data behind Tableau dashboard'
+subject = 'ABC'
 
 msg = MIMEMultipart()
 msg['From'] = email_user
@@ -25,9 +22,7 @@ msg['Subject'] = subject
 
 body = '''Hey Team,
 
-Please find attached the FST scanner training data
-
-This is an automated email. For any questions regarding the data, please reach out to Rose.
+Please find attached 
 
 Thank you'''
 
@@ -39,7 +34,7 @@ attachment = open(file_name, 'rb')
 part = MIMEBase('application', 'octet-stream')
 part.set_payload(attachment.read())
 encoders.encode_base64(part)
-part.add_header('Content-Disposition', "attachment; filename= " + "FST Detail Information Data Extract.xlsx")
+part.add_header('Content-Disposition', "attachment; filename= " + "Filename.xlsx")
 
 msg.attach(part)
 text = msg.as_string()
