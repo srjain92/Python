a4c738bce526fdbaab5c1514bfa32f31f0a83228
Saurabh Jain <saurabhjainjs@gmail.com>
2020-06-09 16:04:37 -0400

    Update UPS.py

--- UPS.py
+++ UPS.py
@@ -12,25 +12,23 @@ conn = pyodbc.connect('Driver={SQL Server};'
                       'UID=abc;'
                       'PWD=xxx;')
 
-script1 =('''SELECT [ID]
-            ,[DDS_CaseID] AS [CaseID]
-            ,CONVERT(DATE,[DDS_ShipDate]) AS [Ship Date]
-            FROM [DDSLab_Services].[dbo].[ShippingEndofDay]
-            WHERE [Deleted] = 0''')
+script1 =('''
+        **********INSERT SQL QUERY***********
+        ''')
 
 df1 = pd.read_sql_query(script1, conn)
 
 params = urllib.parse.quote_plus('Driver={SQL Server};'
-                                 'Server=dds-sql03;'
-                                 'Database=DLPLUS;'
+                                 'Server=Server Name;'
+                                 'Database=Database Name;'
                                  'Trusted_Connection=yes;')
 
 conn = sqlalchemy.create_engine('mssql+pyodbc:///?odbc_connect=%s' % params)
 connection = conn.connect()
-truncate_query = sqlalchemy.text("TRUNCATE TABLE DDSNet_UPS_IPD")
+truncate_query = sqlalchemy.text("TRUNCATE TABLE TABLENAME")
 connection.execution_options(autocommit=True).execute(truncate_query)
 
-df1.to_sql(name='DDSNet_UPS_IPD', con=conn, if_exists='append', index=False, method="multi", chunksize=100)
+df1.to_sql(name='TABLENAME', con=conn, if_exists='append', index=False, method="multi", chunksize=100)
 
 print('Task Completed')
 
