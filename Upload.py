83a93a6649cb2a65aeff42a85747fdac134ce516
Saurabh Jain <saurabhjainjs@gmail.com>
2020-06-09 15:30:02 -0400

    Update Upload.py

--- Upload.py
+++ Upload.py
@@ -1,18 +1,14 @@
+#Code to upload an excel file in the database
 import pandas as pd
 import xlrd
 import pyodbc
 import sqlalchemy
 import urllib
 
-x1 = pd.ExcelFile(r'\\dds-file04\users\Saurabh.Jain\Excel\FedEx IPD Data.xlsx')
+x1 = pd.ExcelFile(r'\\filename.xlsx')
 x1 = x1.sheet_names[0]
 
-df = pd.read_excel(r'\\dds-file04\users\Saurabh.Jain\Excel\FedEx IPD Data.xlsx', x1)
-
-# try:
-#     df = pd.read_excel(r'\\dds-file04\users\Saurabh.Jain\Excel\FedEx IPD Data.xlsx', 'Sheet1')
-# except:
-#     df = pd.read_excel(r'\\dds-file04\users\Saurabh.Jain\Excel\FedEx IPD Data.xlsx', 'Sheet2')
+df = pd.read_excel(r'\\filename.xlsx', x1)
 
 df.rename(columns={
     'Shipment Tracking Number': 'TrackingNumber'
@@ -22,12 +18,12 @@ df.rename(columns={
 # df['Shipment Delivery Date'] = pd.to_datetime(df['Shipment Delivery Date'])
 
 params = urllib.parse.quote_plus('Driver={SQL Server};'
-                                 'Server=dds-sql03;'
-                                 'Database=DLPLUS;'
+                                 'Server=Server Name;'
+                                 'Database=Database Name;'
                                  'Trusted_Connection=yes;')
 
 conn = sqlalchemy.create_engine('mssql+pyodbc:///?odbc_connect=%s' % params)
 
-df.to_sql(name='DDSNet_Fedex_IPD', con=conn, if_exists='append', index=False, method="multi", chunksize=100)
+df.to_sql(name='File Name', con=conn, if_exists='append', index=False, method="multi", chunksize=100)
 
 print('Task Completed')
